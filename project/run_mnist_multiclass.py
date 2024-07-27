195bbb8e8a8e8ca5c8655a1f1d249dc331deb524
Tyler Poon <tylerpoon@gmail.com>
2024-07-27 01:06:34 -0700

    done

--- project/run_mnist_multiclass.py
+++ project/run_mnist_multiclass.py
@@ -41,8 +41,7 @@ class Conv2d(minitorch.Module):
         self.bias = RParam(out_channels, 1, 1)
 
     def forward(self, input):
-        # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        return minitorch.conv2d(input, self.weights.value) + self.bias.value
 
 
 class Network(minitorch.Module):
@@ -67,12 +66,23 @@ class Network(minitorch.Module):
         self.mid = None
         self.out = None
 
-        # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        self.conv1 = Conv2d(1, 4, 3, 3)
+        self.conv2 = Conv2d(4, 8, 3, 3)
+        self.linear1 = Linear(392, 64)
+        self.fc = Linear(64, C)
 
     def forward(self, x):
-        # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        x = self.conv1(x).relu()
+        self.mid = x
+        x = self.conv2(x).relu()
+        self.out = x
+        x = minitorch.maxpool2d(x, (4, 4))
+        x = self.linear1(x.view(BATCH, 392)).relu()
+        x = minitorch.dropout(x, 0.25, self.mode == "eval")
+        x = self.fc(x)
+        x = minitorch.logsoftmax(x, dim=-1)
+        return x
+
 
 
 def make_mnist(start, stop):
